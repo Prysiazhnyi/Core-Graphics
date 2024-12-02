5e361878bc77ea40c8ae7af64d1ef96fa82c38b6
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-12-02 17:21:40 +0200

    Initial Commit

---
+++ Project-27_Core Graphics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-27_Core Graphics
+//
+//  Created by Serhii Prysiazhnyi on 02.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
